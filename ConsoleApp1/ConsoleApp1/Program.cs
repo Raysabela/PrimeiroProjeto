f73321c8f513a02cd534b3469c527daba9158721
rafaelaalvesn <rafaelaalvesn@hotmail.com>
2020-04-04 17:53:04 -0300

    Criação método para somar

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Comentários
+
+            /*       
+            int é para número
+            string é palavra
+            DateTime para data
+            double para número fracionado   
+             */
+
+            int numero1;
+            int numero2;
+            int resultado;
+
+            Console.WriteLine("Digite o primeiro numéro");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numéro");
+            numero2 = int.Parse(Console.ReadLine());
+
+            resultado = SomaDoisNumerosInteiros(numero1, numero2);
+            Console.WriteLine("O resultado é: " + resultado);
+            Console.ReadKey();
+
+
+        }
+
+        public static int SomaDoisNumerosInteiros(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        public static string RetornaNomeDePessoa()
+        {
+            return "Rafaela";
         }
     }
 }
